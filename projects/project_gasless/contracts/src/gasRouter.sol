d578e1b54549125e5f40f5ccc997b8002251a7aa
0xfdbu <neomoney77@protonmail.com>
2025-09-04 15:22:20 +0200

    added optimized contract

--- projects/project_gasless/contracts/src/gasRouter.sol
+++ projects/project_gasless/contracts/src/gasRouter.sol
@@ -7,7 +7,7 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 
 contract GasTopUp is Ownable, ReentrancyGuard {
     using SafeERC20 for IERC20;
