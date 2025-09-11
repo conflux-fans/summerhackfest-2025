9b56d87649379c970da8756acf569913def2897b
0xfdbu <neomoney77@protonmail.com>
2025-09-11 11:38:19 +0200

    deleted old projects

--- projects/project_eip7702/contracts/ignition/modules/tokenTransferDelegate.ts
+++
@@ -1,9 +0,0 @@
-import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
-
-export default buildModule("TokenTransferDelegateModule", (m) => {
-  // Deploy the TokenTransferDelegate contract
-  const tokenTransferDelegate = m.contract("TokenTransferDelegate");
-
-  // Return the deployed contract for reference
-  return { tokenTransferDelegate };
-});
--- projects/project_gasless/app/src/pages/HomePage.tsx
+++
@@ -1,9 +0,0 @@
-
-import { GasTopUp } from "../components/Interactions/swapToCfx";
-export function HomePage() {
-  return (
-    <div className="container-xxl">
-            <GasTopUp />
-    </div>
-  )
-}
